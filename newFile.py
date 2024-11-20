be7ed7f0c8af38a00f941bf5f9ac4dabe5e0c77e
arminsfuton5w4 <gracehua@andrew.cmu.edu>
2024-11-19 23:28:42 -0500

    Tuesday11/19

--- newFile.py
+++ newFile.py
@@ -3,9 +3,15 @@ import random
 import copy
 
 class Waitress:
-    def __init__(self, x, y, length, width):
+    def __init__(self, x, y):
         self.x, self.y = x,y
-        self.length, self.width =length, width
+    
+    def move(self):
+        self.y-=3
+    
+    def draw(self):
+        drawRect(self.x, self.y,50,20, fill='purple')
+
 
 class Customer:
     def __init__(self, x, y, length, width):
@@ -14,40 +20,41 @@ class Customer:
         
     def customerOrder(self):
         return generateOrder(menu, allToppings)
+    
+    def draw(self):
+        drawRect(self.x, self.y, 50,20, fill='red')
 
-class Customers:
-    def __init__(self, customerList):
-        self.customers=[]
-        for (x,y,length, width) in customerList:
-            self.customers.append(Customer(x,y,length, width))
+# class Customers:
+#     def __init__(self, customerList):
+#         self.customers=[]
+#         for (x,y,length, width) in customerList:
+#             self.customers.append(Customer(x,y,length, width))
 
 def distance(x0,y0,x1,y1):
     return (((x1-x0)**2+(y1-y0)**2)**0.5)
 
 menu =['cake-roll', 'milk-tea', 'sunday', 'crepe-cake']
 flavors={'fruits': {'strawberry', 'peach', 'mango'},'others': {'matcha', 'chocolate', 'ube', 'red-bean'}}
-allToppings=['strawberry', 'peach', 'mango', 'matcha', 'chocolate', 'ube', 'red-bean']
+allToppings=['strawberry', 'mango', 'matcha', 'chocolate', 'ube', 'red-bean']
 
 
 def generateOrder(menu, allToppings):
     toppings=copy.copy(allToppings)
-    topping1=allToppings[random.randint(0,7)]
+    topping1=allToppings[random.randint(0,len(allToppings))]
     toppings.remove(topping1)
-    topping2=toppings[random.randint(0,7)]
-    return f'{topping1} {topping2} {menu[random.randint(0,3)]}'
+    topping2=toppings[random.randint(0,len(allToppings))]
+    return f'{topping1} {topping2} {menu[random.randint(0,len(menu))]}'
 
 def makeDish(order):
     t1, t2, base=getIngredients(order)
-    
+
 
 def getIngredients(order):
     items=order.split()
     topping1=items[0]
     topping2=items[1]
     base=items[2]
-    return (topping1, topping2, base)
-
-
+    return (topping1, topping2, base) 
 
 
 #table locations & boundaries
@@ -58,21 +65,27 @@ def getIngredients(order):
 #MODEL
 def onAppStart(app):
     app.width, app.height = 800,500
+    app.customers=[]
     app.isCooking=False
 
 #VIEW
-def drawTable(app):
-    drawRect(200,200,50,50, fill='black')
+def drawTable(app):          
+    for i in range(3):
+        drawRect(200+i*100,200,50,50, fill='black')
+    
+def drawOrders(app):
+    drawRect(375, 50, 150, 160, fill='blue', opacity=30)
 
 
 def redrawAll(app):
-    pass
+    drawTable(app)
+    drawOrders(app)
+    for 
 
 def main():
     runApp()
 
 main()
 #CONTROLLER
-def onMouseDrag(app, mouseX, mouseY):
 
 cmu_graphics.run()
