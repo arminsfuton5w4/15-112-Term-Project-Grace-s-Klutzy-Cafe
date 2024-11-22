34bde83fd054d69159d203e68be16b4514bbf6bb
arminsfuton5w4 <gracehua@andrew.cmu.edu>
2024-11-21 20:12:23 -0500

    dfs

--- primary.py
+++ primary.py
@@ -41,6 +41,32 @@ flavors={'fruits': {'strawberry', 'peach', 'mango'},'others': {'matcha', 'chocol
 allToppings=['strawberry', 'mango', 'matcha', 'chocolate', 'ube', 'red-bean']
 
 
+def DFS(adjacencyList, state, s, v):
+    b,target,L=state
+    if v in s:
+        return revisit(state, v)
+    else:
+        visit(state, v)
+        s.add(v)
+        for neighbor in board[v]:
+            DFS(adjacencyList, L, s, neighbor)
+        finish(state, v)
+
+def visit(state, v):
+    b,target,L=state
+    if not b:
+        L.append(v)
+    if v in target:
+        b=True
+    else:
+        return (b, target, L)
+    
+def revisit(state, v):
+    return state
+
+def finish(state, v):
+    return state
+
 def generateOrder(menu, allToppings):
     toppings=copy.copy(allToppings)
     topping1=allToppings[random.randint(0,len(allToppings))]
@@ -50,7 +76,6 @@ def generateOrder(menu, allToppings):
 
 def makeDish(order):
     t1, t2, base=getIngredients(order)
-
 
 def getIngredients(order):
     items=order.split()
@@ -67,7 +92,7 @@ def getIngredients(order):
 def onAppStart(app):
     app.width, app.height = 800,500
     app.rows, app.cols=10,16
-    app.board=makeBoard(app)
+    app.board=adjacencyList()
 
     app.customers=[]
     app.isCooking=False
@@ -77,19 +102,38 @@ def onAppStart(app):
 
     app.foodX, app.foodY=100,50
 
-def makeBoard(app):
-    board=[([0] * 16) for row in range(10)]
-    newB=[[10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-           [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-           [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-           [10, 10, 10, 10, 10, 10, 10, 10, 10, 0, 0, 0, 0, 0, 10, 10],
-           [10, 10, 10, 10, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 10, 10],
-           [10, 10, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [10, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [10, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-           [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]]
-    return newB
+def makeAdjacencyList():
+    adjList=dict()
+    for row in range(4):
+        for col in range(12):
+            #four edge case: corners: 2 edges
+            if row==0 and col==0:
+                adjList[(row,col)]={(row+1, col), (row, col+1)}
+            elif row==0 and col==11:
+                adjList[(row,col)]={(row+1, col), (row, col-1)}
+            elif row==3 and col==0:
+                adjList[(row,col)]={(row-1, col), (row, col+1)}
+            elif row==3 and col==11:
+                adjList[(row,col)]={(row-1, col), (row, col-1)}
+            #sides of the board: 3 edges
+            elif row==0:
+                adjList[(row,col)]={(row+1, col), (row, col-1), (row, col+1)}
+            elif col==0:
+                adjList[(row,col)]={(row-1, col), (row+1, col), (row, col+1)}
+            elif row==3:
+                adjList[(row,col)]={(row-1, col), (row, col-1), (row, col+1)}
+            elif col==11:
+                adjList[(row,col)]={(row-1, col), (row+1, col), (row, col-1)}
+            #everything else: 4 edges
+            else:
+                adjList[(row, col)]={(row-1, col), (row+1, col), (row, col-1), (row, col+1)}
+    return adjList
+
+board=makeAdjacencyList()
+tables=[(0,4),(0,5),(1,4),(1,5), (2,1),(2,2),(3,1),(3,2), (2,7),(2,8),(3,7),(3,8)]
+visited=set()
+state=(False, tables, [])
+print(DFS(board, state, visited, (0,9)))
 
 #VIEW
 def drawTable(app):
