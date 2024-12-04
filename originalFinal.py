57a080486b321cfe81372c812c4c18309e767146
arminsfuton5w4 <gracehua@andrew.cmu.edu>
2024-12-04 18:04:07 -0500

    yes

--- originalFinal.py
+++ originalFinal.py
@@ -71,7 +71,6 @@ class Layout:
         if (y, x) == target:
             other.isAtExit=True
 
-# layout2=Layout([(0,4),(0,5),(1,4),(1,5), (2,1),(2,2),(3,1),(3,2), (2,7),(2,8),(3,7),(3,8)])
 layout=Layout([(1,4),(3,2),(3,8)])
 
 class Customer:
